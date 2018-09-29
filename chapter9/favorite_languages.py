57e0aa0a096731e47c23c86f2504ec8cd5612699
Xiaodong Li <bryne_lxd@sina.com>
2018-09-29 23:18:51 +0800

    demo of OrderedDict class added

---
+++ chapter9/favorite_languages.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+from collections import OrderedDict
+
+favorite_languages = OrderedDict()
+
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorate language is " +
+          language.title() + ".")
+
