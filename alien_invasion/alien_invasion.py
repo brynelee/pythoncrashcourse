0eee8817e13f5fa1708bbc8ed0b4d135765dcf61
Xiaodong Li <bryne_lxd@sina.com>
2018-10-02 01:07:50 +0800

    ship movement done

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -15,11 +15,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:
 
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
